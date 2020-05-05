b35235e7663f92065430157bcbe8d0fa759f75ce
unknown <bradleypelton@gmail.com>
2020-05-04 23:57:12 -0400

    add_item_to_cart, uuid and auth token handling

--- core_swarm.py
+++ core_swarm.py
@@ -2,10 +2,11 @@ from collections import defaultdict
 import csv
 import random
 import base64
+import uuid
 
 from locust import HttpLocust, TaskSet, task, between, seq_task
 
-users_dict = defaultdict(dict)
+users_dict = defaultdict(dict)  # TODO- switch to an ordered dict?
 with open('users.csv', 'r') as bccsv:
     reader = csv.reader(bccsv, delimiter=',', quotechar='|')
     for row in reader:
@@ -15,6 +16,8 @@ print(users_dict)
 
 
 class UserBehaviour(TaskSet):
+    # IT IS a best practice to add timeouts to every request.
+    # TODO- refactor all requests to have timeouts 
     def setup(self):
         """setup the taskset class"""
         pass
@@ -28,6 +31,8 @@ class UserBehaviour(TaskSet):
         random_user = users_dict.popitem()
         self.username = random_user[0]
         self.password = random_user[1]['password']  # the value(a dict) is the second object
+        self.uuid = str(uuid.uuid1())  # UUID to be used multiple places as a payload item
+
         # TODO- add proxy logic here
 
     def on_stop(self):
@@ -41,7 +46,7 @@ class UserBehaviour(TaskSet):
 
     @seq_task(2)
     def login(self):
-        """login"""
+        """login. TODO- refactor with a custom request.auth.AuthBase class?"""
         login_post_request = dict()  # build a payload with user credentials
         login_post_request['username'] = self.username
         # password needs to be encoded to base 64
@@ -49,18 +54,24 @@ class UserBehaviour(TaskSet):
         encoded_password = encoded_password.decode()  # Convert from binary back to string
         login_post_request['password'] = encoded_password
         r = self.client.post("/login", json=login_post_request)
-        # self.auth_token = r['Auth_token']
         assert 'Auth_token:' in r.text, f"Login failed for {self.username}"
-        # print(f"status code for {self.username} login is {r.status_code}")
-        # print(r.content)
+
+        self.auth_token = r.json().split(": ")[-1]
 
     @seq_task(3)
     def add_item_to_my_cart(self):
-        """I need an ID, and a cookie"""
-        # example payload 
-        # {"id":"4104ee74-7238-8b70-0fcf-e3a5f14293cd"
-        # ,"cookie":"dGVzdGxvY3VzdDAxMTU4OTExOQ==","prod_id":1,"flag":true}
+        """ Add a random item to my cart by sending a post request"""
+        # TODO- UNTESTED INSIDE LOCUST CLASS (tested standalone)
+        # Need to construct a payload 
+        payload = {}
+        payload["cookie"] = self.auth_token
+        payload["prod_id"] = random.randint(1, 15)  # 15 items in the database
+        payload["id"] = self.uuid
+        payload["flag"] = 'true'
+        new_item_post = s.post('https://api.demoblaze.com/addtocart', json=payload)
 
+        assert new_item_post.status_code = 200
+        assert new_item_post.content = b''
     # @seq_task(4)
     # def get_cart_info(self):
     #     """Print out the number of items in the cart and the total price"""
@@ -85,7 +96,41 @@ class WebsiteUser(HttpLocust):
     wait_time = between(2, 3)
     host = 'https://api.demoblaze.com'
 
-# r = requests.post('https://api.demoblaze.com/login', json= {"username": "testlocust01", "password":"Turing123"})
+
+import requests
+s = requests.Session()
+s.headers.update({'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36\
+(KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36'})
+
+# LOGIN POST REQUEST, WORKING
+r = s.post('https://api.demoblaze.com/login', json={
+    "username": "testlocust01", "password": "VHVyaW5nMTIz"})
+
+# ADD ITEM TO CART POST REQUEST, WORKING
+payload = {}
+payload["cookie"] = r.json().split(": ")[-1]
+payload["prod_id"] = 1
+payload["id"] = str(uuid.uuid1)
+payload["flag"] = 'true'
+new_item_post = s.post('https://api.demoblaze.com/addtocart', json=payload)
+
+
+# EXAMPLE 
+# user=15b052bb-14cf-27c5-b923-559ca30046ee
+# HERE IS HOW THE UUID/GUID IS BEING GENERATED
+# function guid() {
+#   function s4() {
+#     return Math.floor((1 + Math.random()) * 0x10000)
+#       .toString(16)
+#       .substring(1);
+#   }
+#   return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+#     s4() + '-' + s4() + s4() + s4();
+# }
+
+# document.cookie = "user=" + guid();
+# https://stackoverflow.com/questions/39232293/how-to-get-a-uuid-with-python-requests
+
 
 # locust -f core_swarm.py --no-web -c 5 -r .5
 # -c (NUMBER_OF_LOCUSTS)
--- requestnotes.py
+++ requestnotes.py
@@ -1,6 +1,10 @@
 import requests
 
-# GET, PUT, POST, DELETE, HEAD, OPTIONS
+
+# https://httpbin.org IS A GREAT WEBSITE FOR TESTING REQUESTS
+
+
+# HTTP VERBS: GET, PUT, POST, DELETE, HEAD, OPTIONS
 # ALL RETURN A RESPONSE OBJECT, r
 r = requests.get('https://api.github.com/events')
 r = requests.put('https://httpbin.org/put', data={'key': 'value'})
@@ -23,3 +27,6 @@ for k, v in vars(r).items():
 url = 'https://api.github.com/some/endpoint'
 headers = {'user-agent': 'my-app/0.0.1'}
 r = requests.get(url, headers=headers)
+
+
+# Many apis don't even respond to request.OPTIONS . Pretty much an old deprecated http verb
